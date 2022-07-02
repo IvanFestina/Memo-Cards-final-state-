aa3b770559eec521f25ae6655d29679262e70f99
IvanFestina <ivanfestina@gmail.com>
2022-07-02 23:59:40 +0300

    final commit /

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import './index.css';
+import App from './ui/App';
+import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
-import {HashRouter} from "react-router-dom";
-import {store} from "./store_BLL/store";
-import {App} from "./App";
+import {store} from "./bll/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>
 );
+
+
+reportWebVitals();
